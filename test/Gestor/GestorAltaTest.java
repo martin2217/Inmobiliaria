74446cd66414f196ba1fd7dbee5cc426ee0fa175
martin2217 <martin_f1g@hotmail.com>
2016-11-10 17:25:30 -0300

    Agregadas clases de TEST .. Diagramadas, pero falta implementación completa

---
+++ test/Gestor/GestorAltaTest.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Gestor;
+
+import Controlador.ConexionUtil;
+import Modelo.Inmueble;
+import org.hibernate.SessionFactory;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Martin
+ */
+public class GestorAltaTest {
+    
+    private static SessionFactory sesion;
+    
+    public GestorAltaTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        sesion = ConexionUtil.getSessionFactory();
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+        sesion.close();
+    }
+    
+    @Before
+    public void setUp() {
+        // Contar cantidad de inmuebles presentes en la BD, para comparar (hacer metodo, o usar el que consulta por inmuebles)
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of altaInmueble method, of class GestorAlta.
+     */
+    @Test
+    public void testAltaInmueble() {
+        System.out.println("Alta de Inmueble 1");
+        
+        
+        // Crear objetos propietario, barrio, cliente, etc para añadir al inmueble
+        
+        
+        /*Inmueble casa = Inmueble aux = new Inmueble(
+                Integer.parseInt(jLabel3.getText()),
+                cli,
+                prop,                
+                barri,
+                jTextCalle.getText(),
+                Integer.parseInt(jTextNumero.getText()),
+                Integer.parseInt(jTextPiso.getText()),
+                Integer.parseInt(jTextDepartamento.getText()),
+                jComboBox4.getSelectedItem().toString(),
+                Float.parseFloat(jTextPrecioVenta.getText()),
+                (String)jComboBox5.getSelectedItem(),
+                Integer.parseInt(jTextFrente.getText()),
+                Integer.parseInt(jTextFondo.getText()),
+                Integer.parseInt(jTextSuperficie.getText()),
+                "alta",
+                Integer.parseInt(jTextMontoReserva.getText()),
+                Integer.parseInt(jTextVigencia.getText()),
+                Integer.parseInt(jTextPrecioVenta.getText()),
+                date2, 
+                //se cargan los campos que se agregaron a inmuebles.
+                Integer.parseInt(jTextDormitorio.getText()),
+                jCheckBoxPropiedadHorizontal.isSelected(),
+                Float.parseFloat( jTextAntiguedad.getText()),
+                Integer.parseInt(jTextBanio.getText()),
+                jCheckBoxGaraje.isSelected(),
+                jCheckBoxPatio.isSelected(),
+                jCheckBoxAguaCorriente.isSelected(),
+                jCheckBoxPiscina.isSelected(),
+                jCheckBoxCloaca.isSelected(),
+                jCheckBoxGasNatural.isSelected(),
+                jCheckBoxPavimento.isSelected(),
+                jCheckBoxLavadero.isSelected(),
+                jCheckBoxAguaCaliente.isSelected(),
+                jTextTelefono.getText(),
+                jTextObservaciones.getText()
+                );*/
+        
+        // Agrega a la BD el inmueble
+        //GestorAlta.altaInmueble(casa);
+        
+        // Contar cantidad de inmuebles en la BD (hacer metodo)
+        
+        // Assert a la cantidad de inmuebles (+1)
+        
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
