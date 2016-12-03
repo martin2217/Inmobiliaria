8edf657242fe7bc8ef0cabcf494881f9ed498bcc
DESKTOP-UU62B20\germa <gerdarpog@gmail.com>
2016-12-03 20:07:13 -0300

    Generación de los elementos necesarios para emitir el Catálogo de Inmuebles. Se cargaron los Plugins y bibliotecas presentes en la carpeta compartida del Drive con el video para su instalacion correcta.

--- src/Reportes/Reportes.java
+++ src/Reportes/Reportes.java
@@ -5,10 +5,42 @@
  */
 package Reportes;
 
+
+import Modelo.ListaCatalogo;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
+
+
 /**
  *
  * @author germa
  */
 public class Reportes {
+    
+    public static void imprimirCatalogo (){
+        List lista = new ArrayList();
+        
+        for(int i=0;i<5;i++)
+        {            
+            ListaCatalogo inmueble_aux = new ListaCatalogo("Santa fe", "Santa fe", "San Telmo", "Depto", Float.parseFloat("120.000"), "Sur", 120, "SI", "SI", "SI", "SI", "SI", "SI");
+            lista.add(inmueble_aux);
+        }
+        
+        JasperReport reporte = (JasperReport)JRLoader.loadObject("catalogo.jasper");
+             
+        Map parametro = new HashMap();        
+        //parametro.put("cliente", "el que sea");
+        JasperPrint jprint =  new JasperFillManager(reporte, parametro, new JRBeanCollectionDataSource(lista) );
+    }
     
 }
